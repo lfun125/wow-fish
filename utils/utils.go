0a4ed27c070f19a3a70dbce80089b7d6ff74bd3f
haha <a@b.com>
2020-12-16 23:42:19 +0800

    smg

--- utils/utils.go
+++ utils/utils.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"image"
+	"image/color"
 	"image/png"
 	"log"
 	"math"
@@ -98,5 +99,63 @@ func averageLuminance(img image.Image) (luminance float64) {
 		y += 0.299*float64(v.R) + 0.587*float64(v.G) + 0.114*float64(v.B)
 	}
 	luminance = y / tol
+	return
+}
+
+func Compared(before, after image.Image) float64 {
+	rgb1 := averageColor(before)
+	rgb2 := averageColor(after)
+	//return math.Abs(float64(rgb1.R)-float64(rgb2.R)) + math.Abs(float64(rgb1.G)-float64(rgb2.G)) + math.Abs(float64(rgb1.B)-float64(rgb2.B))
+	return RGBDistance(rgb1, rgb2)
+}
+
+func averageColor(img image.Image) (rgba color.RGBA) {
+	rect := img.Bounds()
+	with := rect.Size().X
+	height := rect.Size().Y
+	//wn := with / 10
+	//hn := height / 10
+	chCoordinate := make(chan [2]int, 1)
+	go func() {
+		for x := 1; x < with; x += 1 {
+			for y := 1; y < height; y += 1 {
+				chCoordinate <- [2]int{x, y}
+			}
+		}
+		close(chCoordinate)
+	}()
+	type Color struct {
+		R, G, B uint32
+	}
+	chResult := make(chan Color)
+	var wt sync.WaitGroup
+	for i := 0; i <= 100; i++ {
+		wt.Add(1)
+		go func() {
+			for v := range chCoordinate {
+				var mc Color
+				mc.R, mc.G, mc.B, _ = img.At(v[0], v[1]).RGBA()
+				mc.R >>= 8
+				mc.G >>= 8
+				mc.B >>= 8
+				chResult <- mc
+			}
+			wt.Done()
+		}()
+	}
+	go func() {
+		wt.Wait()
+		close(chResult)
+	}()
+	var tr, tg, tb, tol uint64
+	for v := range chResult {
+		tol++
+		tr += uint64(v.R)
+		tg += uint64(v.G)
+		tb += uint64(v.B)
+	}
+	rgba.R = uint8(tr / tol)
+	rgba.G = uint8(tg / tol)
+	rgba.B = uint8(tb / tol)
 	return
 }
